4d6646efc5e6840196bbe5e538c689f5eddc1477
Valeria Alvarez G <vale.agiraldo@gmail.com>
2021-11-04 19:15:17 -0500

    Add link to home in /admin/usuario

--- pages/admin/usuarios/index.tsx
+++ pages/admin/usuarios/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Link from 'next/link'
 
 const index = () => {
     return(
         <div>
-            Pagina de admin de usuarios
+            <div>Pagina de admin de usuarios</div> 
+            <Link>
+            <a href="/">Ir al home</a>
+            </Link>
         </div>
     )
 }
